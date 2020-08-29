c64078f8dcc87a7e0058d2aa7b60efde38ec76b9
Haris Pasha Dipto <haris.dipto@gmail.com>
2020-08-29 20:20:18 +0600

    Basic types are added

--- types.js
+++ types.js
@@ -1,7 +1,15 @@
 var myString;
 var myNum;
 var myBool;
+var myVar;
+var strArr;
+var numArr;
+var boolArr;
 myString = 'Hello World';
 myNum = 22;
 myBool = true;
-console.log(myNum);
+myVar = 1;
+strArr = ['Hello', 'World'];
+numArr = [1, 2, 3];
+boolArr = [true, false, true];
+console.log(strArr);
--- types.ts
+++ types.ts
@@ -1,9 +1,20 @@
-let myString: String
-let myNum: Number
-let myBool: Boolean
+let myString: string
+let myNum: number
+let myBool: boolean
+let myVar: any
+
+let strArr: string[]
+let numArr: number[]
+let boolArr: boolean[]
 
 myString = 'Hello World'
 myNum = 22
 myBool = true
+myVar = 1
 
-console.log(myNum)
+
+strArr = ['Hello', 'World']
+numArr = [1, 2, 3]
+boolArr = [true, false, true]
+
+console.log(strArr)
