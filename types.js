90be1604f1cfbeb050361bb10383409e2c14263f
Haris Pasha Dipto <haris.dipto@gmail.com>
2020-08-29 20:24:55 +0600

    Basic types are added

--- types.js
+++ types.js
@@ -2,9 +2,13 @@ var myString;
 var myNum;
 var myBool;
 var myVar;
+// let strArr: string[]
+// let numArr: number[]
+// let boolArr: boolean[]
 var strArr;
 var numArr;
 var boolArr;
+var strNumTuple;
 myString = 'Hello World';
 myNum = 22;
 myBool = true;
@@ -12,4 +16,8 @@ myVar = 1;
 strArr = ['Hello', 'World'];
 numArr = [1, 2, 3];
 boolArr = [true, false, true];
-console.log(strArr);
+strNumTuple = ['Hello', 1, 2, 3];
+var myVoid = null;
+var myNull = null;
+var myUndefined = undefined;
+console.log(myVoid);
--- types.ts
+++ types.ts
@@ -3,9 +3,15 @@ let myNum: number
 let myBool: boolean
 let myVar: any
 
-let strArr: string[]
-let numArr: number[]
-let boolArr: boolean[]
+// let strArr: string[]
+// let numArr: number[]
+// let boolArr: boolean[]
+let strArr: Array<string>
+let numArr: Array<number>
+let boolArr: Array<boolean>
+
+let strNumTuple: [string, number]
+
 
 myString = 'Hello World'
 myNum = 22
@@ -17,4 +23,10 @@ strArr = ['Hello', 'World']
 numArr = [1, 2, 3]
 boolArr = [true, false, true]
 
-console.log(strArr)
+strNumTuple = ['Hello', 1, 2, 3]
+
+let myVoid: void = null
+let myNull: null = null
+let myUndefined: undefined = undefined
+
+console.log(myVoid)
